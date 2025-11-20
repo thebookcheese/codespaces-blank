8927d0ffbb2f0d5134890e7c12956760faee88fc
thebookcheese <160770461+thebookcheese@users.noreply.github.com>
2025-11-20 13:12:01 +0000

    It works!!!!

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
-import Person;
 
 
 public class Main {
--- java/Person.java
+++ java/Person.java
@@ -1,5 +1,3 @@
-package java;
-
 public class Person {
     public int Age = 5;
-}
+};
